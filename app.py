8692e806c3c23c403b4332d1eeb14d8ad6fd8a2c
Sarvesh Chhabra <sarveshpoker@gmail.com>
2025-08-18 02:06:57 +0530

    Correcting some code error

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ from FoodDeliveryTimePrediction.Utils.main_utils import TimePredictionModel
 import streamlit as st
 import pandas as pd
 
-model = load_object('final_model\model.pkl')
-preprocessor = load_object('final_model\preprocessor.pkl')
+model = load_object(r'final_model\model.pkl')
+preprocessor = load_object(r'final_model\preprocessor.pkl')
 final_model=TimePredictionModel(preprocessor=preprocessor,model=model)
     
 
